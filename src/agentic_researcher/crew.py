002bbe2467983af1b0caeca7c823544f14520f2c
Suraj Karki <suraj.karki@uni-bielefeld.de>
2025-12-05 12:38:13 +0100

    feat: updated tasks

--- src/agentic_researcher/crew.py
+++ src/agentic_researcher/crew.py
@@ -2,7 +2,7 @@ from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
 from crewai.agents.agent_builder.base_agent import BaseAgent
 from crewai_tools import SerperDevTool, ScrapeWebsiteTool
-from .tools.scientific_tools import ScientificSearchTool, ResearchSummarizerTool, CitationFormatterTool
+from .tools.scientific_tools import ScientificSearchTool
 from typing import List
 # If you want to run a snippet of code before or after the crew starts,
 # you can use the @before_kickoff and @after_kickoff decorators
